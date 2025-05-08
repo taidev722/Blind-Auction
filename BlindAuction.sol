d0242f1ed25d250131a65fc55a11bb5fa89c305c
tai <taidev722@gmail.com>
2025-05-08 03:05:58 +0200

    complete contract

--- BlindAuction.sol
+++ BlindAuction.sol
@@ -41,7 +41,70 @@ contract BlindAuction {
     ) {
         beneficiary = beneficiaryAddress;
         biddingEnd = biddingTime;
-        revealEnd = biddingEnd + revealTime;
+        revealEnd = biddingEnd;
     }
 
+    function bid(bytes32 blinded) external payable onlyBefore(biddingEnd) {
+        bids[msg.sender].push(Bid({blindedBid: blinded, deposit: msg.value}));
+    }
+
+    function reveal(
+        uint[] calldata values,
+        bool[] calldata fakes,
+        bytes32[] calldata secrets
+    ) external onlyAfter(biddingEnd) onlyBefore(revealEnd) {
+        uint length = bids[msg.sender].length;
+        require(values.length == length);
+        require(fakes.length == length);
+        require(secrets.length == length);
+
+        uint refund;
+
+        for (uint i = 0; i < length; i ++) {
+            Bid storage bidToCheck = bids[msg.sender][i];
+            (uint value, bool fake, bytes32 secret) = (values[i], fakes[i], secrets[i]);
+
+            if (bidToCheck.blindedBid != keccak256(abi.encodePacked(value, fake, secret))) {
+                continue;
+            }
+
+            refund += bidToCheck.deposit;
+
+            if (!fake && bidToCheck.deposit >= value) {
+                if (placeBid(msg.sender, value)) refund -= value;
+            }
+
+            bidToCheck.blindedBid = bytes32(0);
+        }
+
+        payable(msg.sender).transfer(refund);
+    }
+
+    function withdraw() external {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            pendingReturns[msg.sender] = 0;
+            
+            payable(msg.sender).transfer(amount);
+        }
+    }
+
+    function auctionEnd() external onlyAfter(revealEnd) {
+        if (ended) revert AuctionEndAlreadyCalled();
+
+        emit AuctionEnded(highestBidder, highestBid);
+        ended = true;
+        beneficiary.transfer(highestBid);
+    }
+
+    function placeBid(address bidder, uint value) internal returns (bool success) {
+        if (value <= highestBid) {
+            return false;
+        }
+
+        highestBid = value;
+        highestBidder = bidder;
+
+        return true;
+    }
 }
